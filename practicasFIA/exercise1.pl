1ca4aeda65419324c11cc3795ee5cfc283294054
Sergio García <garciparedes@gmail.com>
2016-04-20 11:13:28 +0200

    Hechas dos primeros ejercicios de la practica 5 de FIA

---
+++ practicasFIA/exercise1.pl
@@ -0,0 +1,26 @@
+/*
+
+Practicas Prolog FIA:
+
+Practica 5:
+    Ejercicio 1
+
+Dominio:
+    Personas
+
+Funciones:
+    inteligente(X)      X es inteligente
+    enseña(X, Y)        X enseña Y
+    esDe(X, Y)          X es de Y
+
+*/
+
+enseña(juan, algoritmos).
+
+enseña(mujerDeJuan, inteligenciaArtificial).
+
+esDe(algoritmos, matematicas).
+
+esDe(inteligenciaArtificial, informatica).
+
+inteligente(X) :- enseña(X, Y), esDe(Y, informatica).
---
+++ practicasFIA/exercise2.pl
@@ -0,0 +1,25 @@
+/*
+
+Practicas Prolog FIA:
+
+Practica 5:
+    Ejercicio 2
+
+Dominio:
+    Personas
+
+Funciones:
+    ayudaA(X, Y)        X ayuda a Y
+    viveEnCasaDe(X,Y)   X vive en casa de Y
+    amigoDe(X,Y)        X es amigo de Y
+    trabajaCon(X,Y)     X trabaja con Y
+
+*/
+
+viveEnCasaDe(X, manolo) :- ayudaA(X, juan).
+
+ayudaA(antonio, X) :- trabajaCon(antonio, X).
+
+trabajaCon(X, juan) :- amigoDe(X, carlos).
+
+amigoDe(antonio, carlos).
